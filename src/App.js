b18e9aefc8674820a3c5931e129e056d4b3a032b
venturads <albert@venturads.com>
2019-08-26 23:12:01 -0700

    move search fun

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { async } from 'q';
 
-const APP_ID = '6e21081a';
+function App() {
+  const APP_ID = '6e21081a';
 const APP_KEY = 'c57e2d06ed57335ecb1393abfa793179';
 const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
 useEffect(() => {
-
+  getRecipe();
 }, []);
 
-function App() {
+const getRecipe = async () => {
+  const response = await fetch('https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}');
+  const data = await response.json();
+  console.log(data);
+}
+
   return (
     <div className="App">
       <form>
