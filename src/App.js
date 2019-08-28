12beef6b45ea902e54bff91df61bd2f506401725
venturads <albert@venturads.com>
2019-08-27 17:10:54 -0700

    added counter to test useeffect usestate

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ const APP_ID = '6e21081a';
 const APP_KEY = 'c57e2d06ed57335ecb1393abfa793179';
 const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
+const [counter, setCounter] = useState(0);
 useEffect(() => {
   getRecipe();
+  console.log("effect count");
 }, []);
 
 const getRecipe = async () => {
@@ -25,6 +27,7 @@ const getRecipe = async () => {
           search
         </button>
       </form>
+      <h1 onClick={() => setCounter(counter +1)}>{counter}</h1>
     </div>
   );
 }
