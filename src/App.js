dfa10d48457149307bafd0209a8fb92c8f0afc25
venturads <albert@venturads.com>
2019-08-27 17:37:03 -0700

    added recipe api title calories image props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { async } from 'q';
+import Recipe from './Recipe';
 
 function App() {
 const APP_ID = '6e21081a';
 const APP_KEY = 'c57e2d06ed57335ecb1393abfa793179';
+
+const [recipes, setRecipts] = useState([]);
 
 useEffect(() => {
   getRecipe();
@@ -14,7 +17,8 @@ useEffect(() => {
 const getRecipe = async () => {
   const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
   const data = await response.json();
-  console.log(data);
+  console.log(data.hits);
+  setRecipts(data.hits);
 } 
 
   return (
@@ -25,6 +29,13 @@ const getRecipe = async () => {
           search
         </button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))};
     </div>
   );
 }
