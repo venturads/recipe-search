33e84cb9a46106b8feede2c5e3271ff927a6b926
venturads <albert@venturads.com>
2019-08-27 17:39:32 -0700

    fix key error for key value pair

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ const getRecipe = async () => {
       </form>
       {recipes.map(recipe => (
         <Recipe
+          key={recipe.recipe.label}
           title={recipe.recipe.label}
           calories={recipe.recipe.calories}
           image={recipe.recipe.image}
