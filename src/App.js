5284bb44a028491011def33d3eb835eae13800bf
venturads <albert@venturads.com>
2019-08-27 17:19:45 -0700

    fix back ticks on api fetch recipes

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import { async } from 'q';
 function App() {
 const APP_ID = '6e21081a';
 const APP_KEY = 'c57e2d06ed57335ecb1393abfa793179';
-const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-const [counter, setCounter] = useState(0);
 useEffect(() => {
   getRecipe();
   console.log("effect count");
 }, []);
 
 const getRecipe = async () => {
-  const response = await fetch('https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}');
+  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
   const data = await response.json();
   console.log(data);
-}
+} 
 
   return (
     <div className="App">
@@ -27,7 +25,6 @@ const getRecipe = async () => {
           search
         </button>
       </form>
-      <h1 onClick={() => setCounter(counter +1)}>{counter}</h1>
     </div>
   );
 }
