02c73265972af92cf4d3c9c7d2ff033f7f8a5465
venturads <albert@venturads.com>
2019-08-27 17:06:49 -0700

    added classnames

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { async } from 'q';
 
 function App() {
-  const APP_ID = '6e21081a';
+const APP_ID = '6e21081a';
 const APP_KEY = 'c57e2d06ed57335ecb1393abfa793179';
 const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
@@ -20,9 +19,9 @@ const getRecipe = async () => {
 
   return (
     <div className="App">
-      <form>
-        <input type='text'></input>
-        <button type='submit'>
+      <form className="search-form">
+        <input className="search-bar" type='text'></input>
+        <button className="search-button" type='submit'>
           search
         </button>
       </form>
