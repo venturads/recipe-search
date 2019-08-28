dc7febf6f7f139703da5c1dbd91cd797d9dff46f
venturads <albert@venturads.com>
2019-08-27 17:54:44 -0700

    fix searc to query when submit button

--- src/App.js
+++ src/App.js
@@ -9,25 +9,32 @@ const APP_KEY = 'c57e2d06ed57335ecb1393abfa793179';
 
 const [recipes, setRecipts] = useState([]);
 const [search, setSearch] = useState('');
+const [query, setQuery] = useState('chicken');
+
 useEffect(() => {
   getRecipe();
   console.log("effect count");
-}, []);
+}, [query]);
 
 const getRecipe = async () => {
-  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
   const data = await response.json();
   console.log(data.hits);
-  setRecipts(data.hits);
+  setRecipts(data.hits);  
 } 
 
 const updateSearch = e => {
   setSearch(e.target.value);
   console.log(search);
 }
+
+const getSearch = e => {
+  e.preventDefault();
+  setQuery(search);
+}
   return (
     <div className="App">
-      <form className="search-form">
+      <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type='text' value={search} onChange={updateSearch}></input>
         <button className="search-button" type='submit'>
           search
@@ -40,7 +47,7 @@ const updateSearch = e => {
           calories={recipe.recipe.calories}
           image={recipe.recipe.image}
         />
-      ))};
+      ))}
     </div>
   );
 }
